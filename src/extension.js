af0cea6d1578cb863da4edd664420effca7d9dca
Chow Loong Jin <hyperair@debian.org>
2023-11-03 17:24:08 +0800

    Convert to new extension class format

--- src/extension.js
+++ src/extension.js
@@ -3,37 +3,40 @@ import { Logger } from './utils.js'
 
 let main, logger
 /*eslint-disable */
-function init() {
-  /* eslint-enable */
-  try {
-    logger = new Logger('Extension')
-    main = new Main()
-    logger.info('Initialized')
-  } catch (err) {
-    logger.error(err)
-  }
-}
 
-/*eslint-disable */
-function enable() {
-  /* eslint-enable */
-  try {
-    logger.info('Enabling extension ...')
-    main.start()
-    logger.info('Enabled')
-  } catch (err) {
-    logger.error(err)
-  }
-}
+export default class OverviewNavigationExtension {
+    constructor() {
+        /* eslint-enable */
+        try {
+            logger = new Logger('Extension')
+            main = new Main()
+            logger.info('Initialized')
+        } catch (err) {
+            logger.error(err)
+        }
+    }
 
-/*eslint-disable */
-function disable() {
-  /* eslint-enable */
-  try {
-    logger.info('Disabling extension ...')
-    main.stop()
-    logger.info('Disabled')
-  } catch (err) {
-    logger.error(err)
-  }
-}
+    /*eslint-disable */
+    enable() {
+        /* eslint-enable */
+        try {
+            logger.info('Enabling extension ...')
+            main.start()
+            logger.info('Enabled')
+        } catch (err) {
+            logger.error(err)
+        }
+    }
+
+    /*eslint-disable */
+    disable() {
+        /* eslint-enable */
+        try {
+            logger.info('Disabling extension ...')
+            main.stop()
+            logger.info('Disabled')
+        } catch (err) {
+            logger.error(err)
+        }
+    }
+};
