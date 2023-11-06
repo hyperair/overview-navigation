7074a12b263a1b57bdff5896769870f5a48649d3
Chow Loong Jin <hyperair@debian.org>
2023-11-06 10:13:17 +0800

    Fix indirect export issue with Gtk.Orientation

--- src/preferences/notebook.js
+++ src/preferences/notebook.js
@@ -1,20 +1,20 @@
-import { Box, Orientation, Label } from 'gi://Gtk'
+import Gtk from 'gi://Gtk'
 import { Widget } from './widget.js'
 
 export var NotebookPage = class NotebookPage extends Widget {
   constructor (name) {
     super(
-      new Box({
+      new Gtk.Box({
         'margin-top': 10,
         spacing: 5
       })
     )
     this.name = name
-    this.parent.set_orientation(Orientation.VERTICAL)
+    this.parent.set_orientation(Gtk.Orientation.VERTICAL)
   }
 
   register (notebook) {
-    const label = new Label({ label: this.name })
+    const label = new Gtk.Label({ label: this.name })
     notebook.append_page(this.parent, label)
   }
 }
